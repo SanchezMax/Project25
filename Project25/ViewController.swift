4db00cc2e4a1806b2dcffe247ac7999b32b2d633
Max Zykin <sanchez.max@yandex.ru>
2024-01-06 16:41:17 +0400

    Initial Commit

---
+++ Project25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by Максим Зыкин on 06.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
